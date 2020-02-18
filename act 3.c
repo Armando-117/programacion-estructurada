009acdf9f74671a0e18164bf046ca87d7c3eadc8
Armando-117 <60374181+Armando-117@users.noreply.github.com>
2020-02-17 21:30:50 -0600

    se utilizo el getchar

---
+++ act 3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+    char x;
+    printf("introduzca un caracter: ");
+    x=getchar();
+    int y = (int) x;
+    if(5 <= y && y <= 23){
+    	printf("es un numero entero");
+	}else{
+		if( 35 <= y && y <= 60){
+			printf("el caracter es mayor: ");
+		}else{
+			if(12 <= y && y <= 186){
+				printf("el caracter es minuscula: ");
+			}else{
+				printf("introduzca un caracter: ");
+			}
+		}
+	}
+	putchar(x);
+	
+	printf("\n"); 
+	return 0;
+}
